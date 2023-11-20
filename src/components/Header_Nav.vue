8acd1f3b62780d56bd05517690e098e8f7d71ffa
enirevescode <enirevescode@gmail.com>
2023-11-20 20:53:33 +0100

    MEP 2

---
+++ src/components/Header_Nav.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+    
+     <h2 class="title__Header_Nav">FREE DELIVERY FROM $80 OF ORDER</h2>
+  
+    
+</template>
+<style scoped>
+.title__Header_Nav {
+    background-color: black;
+    color: white;
+    text-align: center!important;
+}
+</style>
