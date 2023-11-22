c968b7dc6e070f5897a6c699e9990552339c29af
enireves <enirevescode@gmail.com>
2023-11-22 10:51:48 +0100

    MEP

--- src/components/Hero_Accueil.vue
+++ src/components/Hero_Accueil.vue
@@ -3,22 +3,42 @@
 </script>
 
 <template>
-    <div class="card" id="Hero_Accueil--img">
-        <Card>
-            <template #title> SUMMER IS HERE </template>
-            <template #content>
-                <p class="m-0">
+    <div id="Hero_Accueil--img">
+        <div class="card__Accueil">
+            <h3 class="ss_titre">SUMMER IS HERE</h3>
+            <h2 class="titre">BIKINI EVERYWHERE</h2>
+            <div>
+                <p class="para__Accueil">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
                     quas!
                 </p>
-            </template>
-        </Card>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
 #Hero_Accueil--img{
    background-image: url('src/assets/img/Piscine.webp');
-   /* object-fit: cover; */
-   height: 100%;
+   height: 800px;
+}
+
+.card__Accueil {
+    padding-top: 400px;
+    padding-left: 200px;
+}
+
+.para__Accueil {
+    max-width: 400px;
+    font-weight: 500;
+}
+.ss_titre {
+    font-size: 30px;
+    font-weight: 700;
+    color: white;
+}
+.titre {
+    font-size: 80px;
+    font-weight: 700;
+    
 }
 </style>
